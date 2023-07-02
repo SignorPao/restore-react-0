55b0f386d21f1ef50b1ad046f4f631dd4345cfa7
Pavel Belousov <signorpao@gmail.com>
2023-07-02 10:43:45 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TopBar from "./components/TopBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <TopBar />
+      <Navbar />
       <h2 className="text-red-500 text-3xl">RECOVERY</h2>
     </div>
   );
