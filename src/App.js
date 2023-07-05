de71961ff0c1b83935fa03dab071c76b601670c6
Pavel Belousov <signorpao@gmail.com>
2023-07-05 22:38:47 +0200

    hero.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import TopBar from "./components/TopBar";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
       <TopBar />
       <Navbar />
-      <h2 className="text-red-500 text-3xl">RECOVERY</h2>
+      <Hero />
+      {/* <h2 className="text-red-500 text-3xl">RECOVERY</h2> */}
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import hero_img from "../assets/hero.jpg";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="w-full h-[90vh]">
+      <img src={hero_img} alt="hero" className="w-full h-full object-cover brightness-75" />
+      <div className="max-w-[1140px] m-auto">
+        <div className="absolute top-[40%] w-full md:w-[50%] max-w-[600px] flex flex-col text-white p-4">
+          <h1 className="font-bold text-4xl lg:text-5xl capitalize">Find your special trip</h1>
+          <h2 className="text-3xl lg:text-4xl py-4 italic">With WEAKAWAY</h2>
+          <p className="lg:text-lg font-light">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto illo
+            quod sunt, possimus sint doloremque labore similique! Quibusdam
+            nesciunt libero blanditiis laudantium, velit placeat dolores
+            ducimus, perferendis porro cumque provident
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
