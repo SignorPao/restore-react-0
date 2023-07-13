309fde9457bae26970375edd789b312b6aa949ca
Pavel Belousov <signorpao@gmail.com>
2023-07-13 21:19:34 +0200

    activities.

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,10 +2,27 @@ import React from "react";
 
 const Activities = () => {
   return (
-    <div>
-      <div>
-        <h3>Resorts</h3>
-        <img src="" alt="" />
+    <div className="max-w-[1140px] m-auto w-full md:flex mt-[-75px]">
+      <div className="relative p-4">
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Resorts</h3>
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-xl"
+          src="https://images.unsplash.com/photo-1561501900-3701fa6a0864?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          alt="resorts"
+        />
+      </div>
+      <div className="relative p-4">
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Cruises</h3>
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-xl"
+          src="https://images.unsplash.com/photo-1559599746-8823b38544c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80"
+          alt="cruises"
+        />
+      </div>
+      <div className="relative p-4">
+        <h3 className="absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold">Excursions</h3>
+        <img className="w-full h-full object-cover relative border-4 border-white shadow-xl"
+          src="https://images.unsplash.com/photo-1682687982360-3fbab65f9d50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80"
+          alt="excursions"
+        />
       </div>
     </div>
   );
