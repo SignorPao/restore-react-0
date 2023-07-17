38822b2bcf8d2d9378801a8e6fe63a13763f0c71
Pavel Belousov <signorpao@gmail.com>
2023-07-17 10:27:32 +0200

    booking.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TopBar from "./components/TopBar";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Activities from "./components/Activities";
+import Booking from "./components/Booking";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Activities />
+      <Booking />
       {/* <h2 className="text-red-500 text-3xl">RECOVERY</h2> */}
     </div>
   );
